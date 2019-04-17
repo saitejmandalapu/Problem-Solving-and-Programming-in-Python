881870ebb7847ca584a370257bcef5dec1d70d1a
MANDALAPU SAI TEJA <24505722+saitejmandalapu@users.noreply.github.com>
2019-04-17 11:16:05 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,19 @@
+#finding Maximumber
+
+#A value
+a=int(input("Enter the value of a"))
+
+#B value
+b=int(input("Enter the value of b"))
+
+#C value
+c=int(input("Enter the value of c"))
+
+
+#finding through IF condition
+if a>b and a>c:
+    print("a is maximum:",a)
+elif b>a and b>c:
+    print("b is Maximum number:",b)
+else :
+    print("c is maximum:",c)
